a835a1f8d625376443ee243c8dda220621208a5b
fatihulil <fatihulil@gmail.com>
2024-12-20 14:10:43 +0700

    chore: update project configuration and dependencies; remove unused files and enhance Tailwind CSS setup

---
+++ configs/layout.config.tsx
@@ -0,0 +1,30 @@
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+
+/**
+ * Shared layout configurations
+ *
+ * you can configure layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <span className=" dark:text-black">
+        FUA24
+      </span>
+    ),
+  },
+  links: [
+    {
+      text: "Documentation",
+      url: "/docs",
+      active: "nested-url",
+    },
+    {
+      text: "Blog",
+      url: "/blog",
+      active: "nested-url",
+    },
+  ],
+};
